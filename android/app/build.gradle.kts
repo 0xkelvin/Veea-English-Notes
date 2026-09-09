d91d9bccd32829ae50907a3c7f4ed7683cc6f26d
0xkelvin <quocvm1@gmail.com>
2026-09-09 23:43:22 +0800

    fix(android): enable core library desugaring and set compileSdk to 37

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -16,10 +16,11 @@ if (keystorePropertiesFile.exists()) {
 
 android {
     namespace = "com.veea.veea_english_app"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 37
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
+        isCoreLibraryDesugaringEnabled = true
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
@@ -62,4 +63,8 @@ android {
 
 flutter {
     source = "../.."
+}
+
+dependencies {
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.4")
 }
